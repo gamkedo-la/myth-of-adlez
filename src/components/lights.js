6a6edf72bb5b83f67bd6423d7a4bf141ae32c3a7
H Trayford <bowman_trayford@hotmail.com>
2023-06-04 15:25:41 -0600

    shadows much improved

--- src/components/lights.js
+++ src/components/lights.js
@@ -8,9 +8,10 @@ function createLights () {
   mainLight.position.set(-20, -30, 50)
   //Set up shadow properties for the light
   // mainLight.shadow.radius = 2.95
-  // mainLight.shadow.mapSize.width = 2048 // default
-  // mainLight.shadow.mapSize.height = 2048 // default
+  mainLight.shadow.mapSize.width = 2048 // default
+  mainLight.shadow.mapSize.height = 2048 // default
   // mainLight.shadow.bias = 0.000001
+  mainLight.shadow.normalBias = 0.1
   mainLight.shadow.camera.near = NEAR_CLIPPING // default
   mainLight.shadow.camera.far = FAR_CLIPPING // default
   mainLight.shadow.camera.left = -16
@@ -18,7 +19,7 @@ function createLights () {
   mainLight.shadow.camera.top = -20
   mainLight.shadow.camera.bottom = 20
   mainLight.tick = () => {
-    // mainLight.position.x += 0.1
+    mainLight.position.x += 0.1
   }
 
   return { ambientLight, mainLight }
